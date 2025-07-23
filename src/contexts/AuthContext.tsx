779e6b45b2e91330faf559b413f616b527d88bf4
Noah <noah@example.com>
2025-07-23 10:38:19 +0100

    unifying sign in callback URL for Google

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -130,7 +130,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     return await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`
+        redirectTo: `${window.location.origin}/radio-m-web/auth/callback`
       }
     });
   };
